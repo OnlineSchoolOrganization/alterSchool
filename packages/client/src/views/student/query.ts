c65a8fedde482bab59685571f16b6beaed66241b
cristichiu <grajdiancristian@gmail.com>
2026-03-21 12:18:29 +0200

    clean up

--- packages/client/src/views/student/query.ts
+++ packages/client/src/views/student/query.ts
@@ -1,1 +1,27 @@
-export { GET_MY_PROFILES } from '../profile/query'
+import { graphql } from "@/api";
+
+export const GET_MY_PROFILES = graphql(`
+  query UserProfiles {
+    user {
+      id
+      profiles {
+        id
+        email
+        username
+        firstName
+        lastName
+        phoneNumber
+        deleted
+        availabilitySlots {
+          id
+          dayOfWeek
+          startTime
+          duration
+        }
+        roleProfile {
+          __typename
+        }
+      }
+    }
+  }
+`)
