ecbae589d30e725de6f5beac0b7b91dbc431ac4c
cristichiu <grajdiancristian@gmail.com>
2026-03-22 15:38:38 +0200

    add global redirect for 404 pages

--- packages/client/src/main.ts
+++ packages/client/src/main.ts
@@ -12,7 +12,13 @@ const app = createApp(App)
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes,
+  routes: [
+    ...routes,
+    {
+      path: '/:pathMatch(.*)*',
+      redirect: '/teacher/list',
+    },
+  ],
 })
 // app.use(createPinia())
 app.provide(DefaultApolloClient, apolloClient)
